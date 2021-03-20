8b3034b89eb03b8068c0be56ad8d4117a794fca1
majorgamerjay <majorgamerjay@protonmail.com>
2021-03-20 13:59:58 +0600

    Remove timestamp feature and improve D-Bus interfaces
    For now, the timestamp feature seems a lot more harder to implement
    especially with the datatypes being so different from D-Bus and Python.
    This is why the timestamp feature will be inside a todo for now.
    
    Wrapped functions that connect the program to D-Bus interfaces into a
    function and that function is invoked every time there is a change or
    error to the program, which may cause the D-Bus interfaces to
    malfunction with outdated data.

--- src/main.py
+++ src/main.py
@@ -4,42 +4,38 @@ from time import sleep
 from collections import deque
 from datetime import timedelta as td
 
+# Connect and get information about player and music using D-Bus
+# The interfaces must be reinitialized again to get the data stored into the
+# variables or the old data will stay and it will give wrong output and errors
+def connect_and_get_dbus_info():
+    # Define global variables for use of variables outside the function
+    global elisa_properties
+    global metadata
+    while True:
+        try:
+            # Proxy object for getting Elisa's Bus.
+            elisa = session_bus.get_object('org.mpris.MediaPlayer2.elisa',
+                '/org/mpris/MediaPlayer2')
+        except dbus.exceptions.DBusException:
+            print('Couldn\'t connect to Elisa\'s Bus')
+            sleep(5)
+        else:
+            break
+    # Properties interface for Elisa
+    elisa_properties = dbus.Interface(elisa, dbus_interface='org.freedesktop.DBus.Properties')
+    # Metadata interface
+    metadata = elisa_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+
 # Declare session bus from D-Bus.
 session_bus = dbus.SessionBus()
 
-# Proxy object for getting Elisa's Bus.
-while True:
-    try:
-        elisa = session_bus.get_object('org.mpris.MediaPlayer2.elisa',
-            '/org/mpris/MediaPlayer2')
-    except dbus.exceptions.DBusException:
-        print('Couldn\'t connect to Elisa\'s Bus')
-        sleep(5)
-    else:
-        break
+elisa_properties = None # Interface for connecting with properties of Elisa's interface
+metadata = None # Getting metadata of currently playing song
 
-# Elisa interface for D-Bus
-elisa_interface = dbus.Interface(elisa, dbus_interface='org.mpris.MediaPlayer2.Player')
-
-# Properties interface for Elisa
-elisa_properties = dbus.Interface(elisa, dbus_interface='org.freedesktop.DBus.Properties')
-
-# Metadata interface
-metadata = elisa_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+connect_and_get_dbus_info()
 
 # Add list of artists to one variable and declare it by popping by dequed
 while True:
-    while True:
-        try:
-            track_length = str(td(microseconds=metadata.get('mpris:length')))
-        except TypeError:
-            print('Could not get length metadata. Please restart the program \
-                    after playing a song.')
-            sleep(5)
-        else:
-            print('Got length metadata of song')
-            break
-
     track_name = metadata.get('xesam:title') or meta.get('xesam:url')
     track_artists = metadata.get('xesam:artist')
 
@@ -48,6 +44,18 @@ while True:
         track_artist = track_artists.popleft()
         while len(track_artists) > 0:
             track_artist = track_artist + ', ' + track_artists.popleft()
+
+    playback_status = ""
+
+    while True:
+        try:
+            playback_status = elisa_properties.Get("org.mpris.MediaPlayer2.Player", "PlaybackStatus")
+        except dbus.exceptions.DBusException:
+            print("Have you closed the program? If you did, restart the program.")
+            connect_and_get_dbus_info()
+            sleep(5)
+        else:
+            break
 
     print(f'{str(elisa_properties.Get("org.mpris.MediaPlayer2.Player", "PlaybackStatus"))}: "{track_name}" by {track_artist}')
 
